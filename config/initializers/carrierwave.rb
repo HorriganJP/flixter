e4850542f0df5535a02a7657d6efdff54ec989f5
JP Horrigan <Horrigan.JP@outlook.com>
2019-07-21 18:27:54 +0000

    Lesson 24 error w/ video displaying/downloading and image displaying.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,9 +5,10 @@ CarrierWave.configure do |config|
   config.aws_bucket = ENV["AWS_BUCKET"] #required
   config.aws_acl = "public-read"
   
-  config.fog_credentials = {
-    provider: 'AWS', #required
+  config.aws_credentials = {
+    aws_access_key_id: ENV["AWS_ACCESS_KEY"], #required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"], #required
+    region: ENV["AWS_REGION"]
   }
 
 
