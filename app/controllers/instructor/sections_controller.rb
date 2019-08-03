70dca143e906e5d5b547326691c54d9ce35f7ae7
JP Horrigan <Horrigan.JP@outlook.com>
2019-08-03 14:37:56 +0000

    Lesson 35 Complete

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,11 +1,9 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course, only: [:new, :create]
+  before_action :require_authorized_for_current_course, only: [:create]
   before_action :require_authorized_for_current_section, only: [:update]
 
-  def new
-    @section = Section.new
-  end
+# removed def new b/c ability to create new section is triggered now on the instructor/courses controller's show action.
 
   def create
     @section = current_course.sections.create(section_params)
